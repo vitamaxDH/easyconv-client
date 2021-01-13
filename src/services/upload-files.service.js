d774f26d200c2626a57739f658d9809a5b5ae188
vitamaxDH <hellonewtry@gmail.com>
2021-01-14 02:44:53 +0900

    feat: axios를 통한 파일전송 설정값 세팅

---
+++ src/services/upload-files.service.js
@@ -0,0 +1,22 @@
+import http from "../http-common";
+
+class UploadFilesService {
+    upload(file, onUploadProgress) {
+        let formData = new FormData();
+
+        formData.append("multipartFile", file);
+
+        return http.post("/api/convert/upload", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            onUploadProgress,
+        });
+    }
+
+    getFiles() {
+        return http.get("/files");
+    }
+}
+
+export default new UploadFilesService();
